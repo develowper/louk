c5a32ec6a0decc937b8effafb7eefb572262c9d5
MMR <moj2raj2@gmail.com>
2025-08-30 00:28:07 +0330

    add test louk

--- inertia/js/mediasoup.ts
+++ inertia/js/mediasoup.ts
@@ -73,11 +73,11 @@ export async function useMediasoup() {
             username: 'turnserver',
             credential: usePage().props.PSWD ?? '',
           },
-          { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:stun1.l.google.com:19302' },
-          { urls: 'stun:stun2.l.google.com:19302' },
-          { urls: 'stun:stun3.l.google.com:19302' },
-          { urls: 'stun:stun4.l.google.com:19302' },
+          // { urls: 'stun:stun.l.google.com:19302' },
+          // { urls: 'stun:stun1.l.google.com:19302' },
+          // { urls: 'stun:stun2.l.google.com:19302' },
+          // { urls: 'stun:stun3.l.google.com:19302' },
+          // { urls: 'stun:stun4.l.google.com:19302' },
         ],
       })
       this.sendTransport.on('connectionstatechange', (state) => {
